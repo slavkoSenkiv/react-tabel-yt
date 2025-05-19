c7da6e0ff16bcd398645ac8f70eddd83f143786e
slavSenkiv <slavko.senkiv@gmail.com>
2025-05-19 21:13:27 +0200

    learn

--- src/components/TaskTable.jsx
+++ src/components/TaskTable.jsx
@@ -1,6 +1,90 @@
-import { Box } from "@chakra-ui/react";
+import { Box } from '@chakra-ui/react';
+import { useState } from 'react';
+import {
+  getCoreRowModel,
+  useReactTable,
+  flexRender,
+} from '@tanstack/react-table';
+import DATA from '../data';
+import EditableCell from './EditableCell';
+
+const columns = [
+  {
+    accessorKey: 'task',
+    header: 'Task',
+    size: 225,
+    cell: EditableCell,
+  },
+  {
+    accessorKey: 'status',
+    header: 'Status',
+    cell: (props) => <p>{props.getValue()?.name}</p>,
+  },
+  {
+    accessorKey: 'due',
+    header: 'Due',
+    cell: (props) => <p>{props.getValue()?.toLocaleTimeString()}</p>,
+  },
+  {
+    accessorKey: 'notes',
+    header: 'Notes',
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+];
 
 const TaskTable = () => {
-  return <Box></Box>;
+  const [data, setData] = useState(DATA);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    columnResizeMode: "onChange",
+    meta: {
+      updateData: (rowIndex, columnId, value) = setData(
+        prev => prev.map(
+          (row, index) => 
+            index === rowIndex ? {
+              ...prev[rowIndex],
+              [columnId] : value
+            }: row
+        )
+      )
+    }
+  });
+
+  console.log(table.getHeaderGroups());
+
+  return (
+    <Box>
+      <Box className="table" w={table.getTotalSize()}>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <Box className="tr" key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <Box className="th" w={header.getSize()} key={header.id}>
+                {header.column.columnDef.header}
+                <Box
+                onMouseDown={header.getResizeHandler()} 
+                onTouchStart={header.getResizeHandler()}
+                  className={`resizer ${
+                    header.column.getIsResizing() ? 'isResizing' : ''
+                  }`}
+                ></Box>
+              </Box>
+            ))}
+          </Box>
+        ))}
+        {table.getRowModel().rows.map((row) => (
+          <Box className="tr" key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <Box className="td" w={cell.column.getSize()} key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </Box>
+            ))}
+          </Box>
+        ))}
+      </Box>
+    </Box>
+  );
 };
 export default TaskTable;
